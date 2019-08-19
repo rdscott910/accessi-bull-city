ce521d821f931373c1817464084218f8601feca1
Ronnie <rscott910@outlook.com>
2019-08-19 12:30:29 -0400

    Add fake data for reviews

--- server/routes/generate_data.js
+++ server/routes/generate_data.js
@@ -10,7 +10,7 @@ router.get('/restaurants', (req, res, next) => {
 	for (let i=0; i < 60; i++) {
 		let rest = new Restaurant();
 		rest.category = category[Math.floor(Math.random() * category.length)]
-		rest.name = faker.name.firstName()
+		rest.name = faker.company.companyName()
 		rest.address = faker.address.city() + ', ' + faker.address.state() + ', ' + faker.address.country()
 		rest.imageURL = faker.image.imageUrl(800, 800, "food")
 		rest.reviews = []
@@ -21,5 +21,19 @@ router.get('/restaurants', (req, res, next) => {
 	}
 	res.status(200).end()
 });
+
+router.get('/reviews', (req, res, next) => {
+	for (let i=0; i<10; i++) {
+		let review = new Review();
+		review.reviewerName = faker.name.firstName()
+		review.reviewContent = faker.lorem.paragraph()
+		review.rating = rating[Math.floor(Math.random() * rating.length)]
+	
+		review.save((err) => {
+			if (err) throw err
+		})
+	}
+	res.status(200).end()
+})
 
 module.exports = router;
