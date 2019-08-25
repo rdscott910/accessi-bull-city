c3d475c723a35c546f7532b92fb091b296c8127c
Ronnie <rscott910@outlook.com>
2019-08-25 07:49:18 -0400

    deployment ready

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,7 +4,7 @@ import { SAVE_REVIEW, FETCH_DATABASE_RESTAURANTS, FETCH_RESTAURANTS, FETCH_RESTA
 const ROOT_URL = 'http://localhost:8000';
 
 export const fetchRestaurants = (location) => dispatch => {
-	axios.get(`${ROOT_URL}/restaurants/location/${location}`)
+	axios.get(`/restaurants/location/${location}`)
 		.then( response => {
 			dispatch({ type: FETCH_RESTAURANTS, payload: response.data});
 		})
@@ -14,7 +14,7 @@ export const fetchRestaurants = (location) => dispatch => {
 }
 
 export const fetchRestaurant = (id) => dispatch => {
-	axios.get(`${ROOT_URL}/restaurants/database/${id}`)
+	axios.get(`/restaurants/database/${id}`)
 		.then(response => {
 			dispatch({ type: FETCH_RESTAURANT, payload: response.data});
 		})
@@ -23,7 +23,7 @@ export const fetchRestaurant = (id) => dispatch => {
 		})
 }
 export const fetchDatabaseRestaurants = () => dispatch => {
-	axios.get(`${ROOT_URL}/restaurants`)
+	axios.get(`/restaurants`)
 		.then( response => {
 			dispatch({ type: FETCH_DATABASE_RESTAURANTS, payload: response.data});
 		})
@@ -32,7 +32,7 @@ export const fetchDatabaseRestaurants = () => dispatch => {
 		})
 }
 export const fetchCurrentRestaurant = (id) => dispatch => {
-	axios.get(`${ROOT_URL}/restaurants/api/${id}`)
+	axios.get(`/restaurants/api/${id}`)
 	.then(response => {
 		dispatch({ type: FETCH_CURRENT_RESTAURANT, payload: response.data});
 	})
@@ -42,7 +42,7 @@ export const fetchCurrentRestaurant = (id) => dispatch => {
 }
 
 export const createReview = (id, updates) => dispatch => {
-	axios.post(`${ROOT_URL}/restaurants/restaurant/update/${id}`, updates)
+	axios.post(`/restaurants/restaurant/update/${id}`, updates)
 	.then( response => {
 		dispatch({ type: CREATE_REVIEW, payload: response.data });
 	})
@@ -51,7 +51,7 @@ export const createReview = (id, updates) => dispatch => {
 	})
 }
 export const saveReview = (newReview) => dispatch => {
-	axios.post(`${ROOT_URL}/reviews/`, {newReview})
+	axios.post(`/reviews/`, {newReview})
 		.then( response => {
 			dispatch({ type: SAVE_REVIEW, payload: response.data });
 		})
@@ -60,7 +60,7 @@ export const saveReview = (newReview) => dispatch => {
 		})
 }
 export const saveRestaurant = (id) => dispatch => {
-	axios.get(`${ROOT_URL}/restaurants/save/${id}`)
+	axios.get(`/restaurants/save/${id}`)
 	.then( response => {
 		dispatch({ type: CREATE_REVIEW, payload: response.data});
 	})
